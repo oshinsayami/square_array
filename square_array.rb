a757940b193b136e9c7a7e91b888a67dd9d74975
Simran <mdrsimran@gmail.com>
2020-10-22 18:33:27 -0500

    commit tree 0f3b8be007f0eb329edc03ec86622f271786cc8a

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
+# require 'pry'
+
 def square_array(array)
   # your code here
+  new_array= []
+  array.each do |n|
+    squared_number= n*n
+    puts "#{squared_number}"
+    new_array.push(squared_number)
+    # binding.pry
+  end
+  new_array
 end
